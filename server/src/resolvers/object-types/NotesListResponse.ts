446e583757928f6d0a1caa5009fda2b52251755c
Thomas Rainford <thomas@rainfords.net>
2021-01-16 14:19:42 +1300

    Create object-type file for a NotesList response.

---
+++ server/src/resolvers/object-types/NotesListResponse.ts
@@ -0,0 +1,24 @@
+import { NotesList } from "../../entities/NotesList";
+import { Field, ObjectType } from "type-graphql";
+
+@ObjectType()
+class Error {
+
+   @Field()
+   property: string
+
+   @Field()
+   message: string
+
+}
+
+@ObjectType()
+export class NotesListResponse {
+
+   @Field(() => NotesList, { nullable: true })
+   notesList?: NotesList
+
+   @Field(() => Error, { nullable: true })
+   errors: Error
+
+}
