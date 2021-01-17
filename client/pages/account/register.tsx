0b8a09a1248c39c2d42b726e2f87eb149d21ff3b
Thomas Rainford <thomas@rainfords.net>
2021-01-18 12:13:49 +1300

    Start register page.

---
+++ client/pages/account/register.tsx
@@ -0,0 +1,18 @@
+import { Box, Center, Flex } from '@chakra-ui/react'
+import React from 'react'
+
+interface Props {
+
+}
+
+const Register = (props: Props) => {
+   return (
+      <Flex mt="50%">
+         <Flex>
+            Register
+         </Flex>
+      </Flex>
+   )
+}
+
+export default Register
