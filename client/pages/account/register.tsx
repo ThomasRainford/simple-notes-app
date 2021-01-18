b0dc9443c8683f31663acb6d4a612fad28295598
Thomas Rainford <thomas@rainfords.net>
2021-01-18 13:37:18 +1300

    Complete UI for register page.

--- client/pages/account/register.tsx
+++ client/pages/account/register.tsx
@@ -1,4 +1,4 @@
-import { Box, Center, Flex } from '@chakra-ui/react'
+import { Box, Button, Center, Divider, Flex, Heading, Input, Stack, Text } from '@chakra-ui/react'
 import React from 'react'
 
 interface Props {
@@ -7,11 +7,23 @@ interface Props {
 
 const Register = (props: Props) => {
    return (
-      <Flex mt="50%">
-         <Flex>
-            Register
-         </Flex>
-      </Flex>
+      <Flex justify="center" align="center" h="100vh" w="100vw" backgroundColor="#488BFF">
+         <Flex direction="column" p="3%" backgroundColor="#fffeee" boxShadow="dark-lg">
+            <Text fontSize="2xl">Create an account</Text>
+            <Center py="4%">
+               <Divider orientation="horizontal" />
+            </Center>
+            <Stack spacing="3" pb="4%">
+               <Text fontSize="sm">Username</Text>
+               <Input placeholder="Enter Username" />
+               <Text fontSize="sm">Email</Text>
+               <Input placeholder="Enter Email" />
+               <Text fontSize="sm">Password</Text>
+               <Input placeholder="Enter Password" />
+            </Stack>
+            <Button colorScheme="blue">Submit</Button>
+         </Flex >
+      </Flex >
    )
 }
 
