23553ead68adb7502a467befb95b828a16e9eb27
Thomas Rainford <thomas@rainfords.net>
2021-01-18 14:10:04 +1300

    Revert "Register / Login page"

--- client/pages/account/register.tsx
+++ client/pages/account/register.tsx
@@ -1,26 +1,17 @@
-import { Box, Button, Center, Divider, Flex, Heading, Input, Stack, Text } from '@chakra-ui/react'
+import { Box, Center, Flex } from '@chakra-ui/react'
 import React from 'react'
-import AccountLayout from '../../components/account/AccountLayout'
 
 interface Props {
 
 }
 
-const Register = ({ }) => {
+const Register = (props: Props) => {
    return (
-      <AccountLayout
-         heading="Create an account"
-         action="Create Account"
-      >
-         <Stack spacing="3" pb="4%">
-            <Text fontSize="sm">Username</Text>
-            <Input placeholder="Enter Username" />
-            <Text fontSize="sm">Email</Text>
-            <Input placeholder="Enter Email" />
-            <Text fontSize="sm">Password</Text>
-            <Input placeholder="Enter Password" />
-         </Stack>
-      </AccountLayout>
+      <Flex mt="50%">
+         <Flex>
+            Register
+         </Flex>
+      </Flex>
    )
 }
 
