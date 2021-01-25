7b2849f2ccdfd16864d9e56dc77926c075b8fd13
Thomas Rainford <thomas@rainfords.net>
2021-01-26 11:19:47 +1300

    Add form for creating a new list.

--- client/components/notes/notes-lists/NewListDrawer.tsx
+++ client/components/notes/notes-lists/NewListDrawer.tsx
@@ -1,5 +1,7 @@
-import { useDisclosure, Button, Drawer, DrawerOverlay, DrawerContent, DrawerCloseButton, DrawerHeader, DrawerBody, Input, DrawerFooter } from '@chakra-ui/react'
+import { Button, Drawer, DrawerOverlay, DrawerContent, DrawerCloseButton, DrawerHeader, DrawerBody, Input, DrawerFooter, FormControl, FormErrorMessage, FormLabel } from '@chakra-ui/react'
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import { CreateListMutationVariables, useCreateListMutation } from '../../../generated/graphql'
 
 interface Props {
    disclosure: any
@@ -9,12 +11,31 @@ interface Props {
 const NewListDrawer: React.FC<Props> = ({ disclosure, btnRef }) => {
 
    const { isOpen, onClose } = disclosure
+   const intialFocusRef = React.useRef()
+
+   const { handleSubmit, errors, register, formState } = useForm()
+
+
+   const [result, executeCreateList] = useCreateListMutation()
+
+   const validateTitle = () => {
+      return true
+   }
+
+   const onSubmit = async (createListInput: CreateListMutationVariables) => {
+
+      const response = await executeCreateList(createListInput)
+
+      console.log(response)
+
+   }
 
    return (
       <Drawer
          isOpen={isOpen}
          placement="left"
          onClose={onClose}
+         initialFocusRef={intialFocusRef}
          finalFocusRef={btnRef}
       >
          <DrawerOverlay>
@@ -23,19 +44,29 @@ const NewListDrawer: React.FC<Props> = ({ disclosure, btnRef }) => {
                <DrawerHeader>Create new Notes List</DrawerHeader>
 
                <DrawerBody>
-                  <Input placeholder="Enter Title..." />
+                  <form onSubmit={handleSubmit(onSubmit)}>
+
+                     <FormControl mb="10%">
+                        <Input
+                           name="title"
+                           placeholder="Title"
+                           autoComplete="off"
+                           ref={register({ validate: validateTitle })}
+                        />
+                        <FormErrorMessage>
+                           {errors.title && errors.title.message}
+                        </FormErrorMessage>
+                     </FormControl>
+
+                     <Button variant="outline" mr={3} onClick={onClose}>
+                        Cancel
+                     </Button>
+                     <Button isLoading={formState.isSubmitting} type="submit" color="blue">
+                        Save
+                     </Button>
+
+                  </form>
                </DrawerBody>
-
-               <DrawerFooter>
-                  <Button variant="outline" mr={3} onClick={onClose}>
-                     Cancel
-                  </Button>
-                  <Button color="blue"
-                     onClick={() => {
-
-                     }}
-                  >Save</Button>
-               </DrawerFooter>
             </DrawerContent>
          </DrawerOverlay>
       </Drawer>
