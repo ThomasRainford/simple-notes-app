6ed49dc9c7167755698ba810939d86e16d276b4f
Thomas Rainford <thomas@rainfords.net>
2021-01-26 15:14:47 +1300

    Add menu components.

--- client/components/notes/notes-lists/SingleList.tsx
+++ client/components/notes/notes-lists/SingleList.tsx
@@ -1,5 +1,5 @@
-import { ChevronRightIcon } from '@chakra-ui/icons'
-import { Flex, IconButton, Text, Tooltip } from '@chakra-ui/react'
+import { ChevronRightIcon, EditIcon, HamburgerIcon, WarningIcon } from '@chakra-ui/icons'
+import { Flex, IconButton, Menu, MenuButton, MenuItem, MenuList, Text, Tooltip } from '@chakra-ui/react'
 import React from 'react'
 import { NotesList } from '../../../generated/graphql'
 
@@ -12,11 +12,26 @@ const SingleList: React.FC<Props> = ({ list, setCurrentList }) => {
    return (
       <Flex justify="space-between" align="center" w="100%" m="5%" >
          <Text>{list.title}</Text>
-         <Tooltip hasArrow label="View Notes" bg="blue.500">
-            <IconButton aria-label="View notes" size="sm" icon={<ChevronRightIcon />}
-               onClick={() => setCurrentList(list)}
-            />
-         </Tooltip>
+         <Flex align="center" justify="flex-end" width="50%">
+            <Menu isLazy>
+               <MenuButton
+                  as={IconButton}
+                  aria-label="Options"
+                  icon={<HamburgerIcon />}
+                  size="sm"
+                  variant="outline"
+               />
+               <MenuList>
+                  <MenuItem icon={<EditIcon />}>Edit Title</MenuItem>
+                  <MenuItem icon={<WarningIcon />}>Delete List</MenuItem>
+               </MenuList>
+            </Menu>
+            <Tooltip hasArrow label="View Notes" bg="blue.500">
+               <IconButton aria-label="View notes" size="sm" ml="5%" icon={<ChevronRightIcon />}
+                  onClick={() => setCurrentList(list)}
+               />
+            </Tooltip>
+         </Flex >
       </Flex >
    )
 }
