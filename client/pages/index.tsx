5725a8c9b5abd6e07ee0d08dd99eca4908807fbf
Thomas Rainford <thomas@rainfords.net>
2021-01-29 15:51:10 +1300

    Links to account pages.

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Flex, Heading, Link } from '@chakra-ui/react'
+import { Button, Flex, Heading, Link, Text } from '@chakra-ui/react'
 import NextLink from 'next/link'
 import Head from 'next/head'
 import React from 'react'
@@ -13,81 +13,40 @@ function Home() {
 
    return (
       <Flex direction="column" justify="center" align="center" w="100vw" h="100vh" bg="#488BFF">
-         <Flex mb="3%">
-            <Heading fontFamily="sans-serif" textColor="white">Simple Notes App</Heading>
+         <Flex direction="column" mb="4%" align="center">
+            <Heading fontFamily="sans-serif" textColor="white" size="3xl">Simple Notes App</Heading>
+            <Text textColor="white" fontSize="lg" mt="2%">Easily create and update notes on the web!</Text>
          </Flex>
          <Flex>
-            <Button bg="white" textColor="#488BFF" mr="5%" size="lg">Login</Button>
-            <Button variant="outline" textColor="white" size="lg" _hover={{ textColor: "#488BFF", backgroundColor: "white" }}>Register</Button>
+            <Text textColor="white" fontSize="lg" fontStyle="italic" mb="4%">Get started now by registering an account.</Text>
+         </Flex>
+         <Flex>
+            <NextLink href="/account/login">
+               <Button
+                  bg="white"
+                  textColor="#488BFF"
+                  mr="5%"
+                  size="lg"
+                  as={Link}
+               >
+                  Login
+            </Button>
+            </NextLink>
+            <NextLink href="/account/register">
+               <Button
+                  variant="outline"
+                  textColor="white"
+                  size="lg"
+                  as={Link}
+                  _hover={{ textColor: "#488BFF", backgroundColor: "white" }}
+               >
+                  Register
+               </Button>
+            </NextLink>
          </Flex>
 
       </Flex>
    )
-   // true ?
-   //   <div className={
-   //     styles.container
-   //   } >
-   //     <Head>
-   //       <title>Create Next App</title>
-   //       <link rel="icon" href="/favicon.ico" />
-   //     </Head>
-
-   //     <main className={styles.main}>
-   //       <NextLink href="/account/login">
-   //         <Link>Login</Link>
-   //       </NextLink>
-   //       <h1 className={styles.title}>
-   //         Welcome to <a href="https://nextjs.org">Next.js!</a>
-   //       </h1>
-
-   //       <p className={styles.description}>
-   //         Get started by editing{' '}
-   //         <code className={styles.code}>pages/index.js</code>
-   //       </p>
-
-   //       <div className={styles.grid}>
-   //         <a href="https://nextjs.org/docs" className={styles.card}>
-   //           <h3>Documentation &rarr;</h3>
-   //           <p>Find in-depth information about Next.js features and API.</p>
-   //         </a>
-
-   //         <a href="https://nextjs.org/learn" className={styles.card}>
-   //           <h3>Learn &rarr;</h3>
-   //           <p>Learn about Next.js in an interactive course with quizzes!</p>
-   //         </a>
-
-   //         <a
-   //           href="https://github.com/vercel/next.js/tree/master/examples"
-   //           className={styles.card}
-   //         >
-   //           <h3>Examples &rarr;</h3>
-   //           <p>Discover and deploy boilerplate example Next.js projects.</p>
-   //         </a>
-
-   //         <a
-   //           href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-   //           className={styles.card}
-   //         >
-   //           <h3>Deploy &rarr;</h3>
-   //           <p>
-   //             Instantly deploy your Next.js site to a public URL with Vercel.
-   //         </p>
-   //         </a>
-   //       </div>
-   //     </main>
-
-   //     <footer className={styles.footer}>
-   //       <a
-   //         href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-   //         target="_blank"
-   //         rel="noopener noreferrer"
-   //       >
-   //         Powered by{' '}
-   //         <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-   //       </a>
-   //     </footer>
-   //   </div> : <div>Loading...</div>
-   // )
 }
 
 export default withUrqlClient(createUrqlClient, { ssr: false })(Home)
