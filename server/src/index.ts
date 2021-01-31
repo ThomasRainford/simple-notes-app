2a63fd8daa54d6acd0740927d3a4c07d31feb48d
Thomas Rainford <thomas@rainfords.net>
2021-02-01 10:54:53 +1300

    Session data stored in mongodb.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,11 @@
 import { MikroORM } from '@mikro-orm/core'
 import { ApolloServer } from "apollo-server-express"
-import connectRedis from 'connect-redis'
+//import connectRedis from 'connect-redis'
 import cors from 'cors'
 import 'dotenv-safe/config'
 import express from "express"
 import session from 'express-session'
-import Redis from 'ioredis'
+//import Redis from 'ioredis'
 import 'reflect-metadata'
 import { buildSchema } from "type-graphql"
 import { COOKIE_NAME, __prod__ } from './constants'
@@ -13,7 +13,8 @@ import ormConfig from './mikro-orm.config'
 import { NotesListResolver } from './resolvers/notesList'
 import { UserResolver } from "./resolvers/user"
 import { OrmContext } from './types/types'
-import { MongoDBStore } from 'connect-mongodb-session'
+import MongoDBStore from 'connect-mongodb-session'
+const MongoStore = MongoDBStore(session)
 
 const main = async () => {
 
@@ -34,10 +35,10 @@ const main = async () => {
    app.use(
       session({
          name: COOKIE_NAME,
-         store: new MongoDBStore({
-            uri: process.env.MONGO_HOST,
+         store: new MongoStore({
+            uri: `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}`,
             databaseName: 'simple-notes-app-db',
-            collection: 'sessions'
+            collection: 'sessions',
          }),
          cookie: {
             maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
@@ -60,7 +61,7 @@ const main = async () => {
          em: orm.em,
          req,
          res,
-         redis,
+         //redis,
       }),
    })
 
